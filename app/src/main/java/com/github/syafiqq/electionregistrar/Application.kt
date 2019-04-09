b503aa5a944896aa7fbf66ed2755be84fa5a7121
Syafiqq <syafiq.rezpector@gmail.com>
2019-04-09 12:28:48 +0700

    Fix logging

--- app/src/main/java/com/github/syafiqq/electionregistrar/Application.kt
+++ app/src/main/java/com/github/syafiqq/electionregistrar/Application.kt
@@ -1,11 +1,27 @@
 package com.github.syafiqq.electionregistrar
 
 import android.app.Application
+import android.util.Log
 import timber.log.Timber
+import timber.log.Timber.DebugTree
+
 
 class Application : Application() {
     override fun onCreate() {
         super.onCreate()
-        Timber.plant(Timber.DebugTree())
+        if (BuildConfig.DEBUG) {
+            Timber.plant(DebugTree())
+        } else {
+            Timber.plant(CrashReportingTree())
+        }
+    }
+
+    private class CrashReportingTree : Timber.Tree() {
+        override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+            if (priority == Log.VERBOSE || priority == Log.DEBUG) {
+                return
+            }
+            super.log(priority, tag, message, t)
+        }
     }
 }
