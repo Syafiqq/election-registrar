8ad457746efe2d8ad2d631e0360c306d999a5051
Syafiqq <syafiq.rezpector@gmail.com>
2019-04-09 12:26:04 +0700

    Plant timber

--- app/src/main/java/com/github/syafiqq/electionregistrar/Application.kt
+++ app/src/main/java/com/github/syafiqq/electionregistrar/Application.kt
@@ -1,5 +1,11 @@
 package com.github.syafiqq.electionregistrar
 
 import android.app.Application
+import timber.log.Timber
 
-class Application : Application()
+class Application : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
+}
