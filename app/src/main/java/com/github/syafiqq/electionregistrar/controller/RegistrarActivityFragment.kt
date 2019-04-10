f1e307ff55b587f0d3d001eefc84ec0683e3f959
Syafiqq <syafiq.rezpector@gmail.com>
2019-04-10 13:51:39 +0700

    Add log

--- app/src/main/java/com/github/syafiqq/electionregistrar/controller/RegistrarActivityFragment.kt
+++ app/src/main/java/com/github/syafiqq/electionregistrar/controller/RegistrarActivityFragment.kt
@@ -8,6 +8,7 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.github.syafiqq.electionregistrar.model.view.QRScannerView
 import com.google.zxing.Result
+import timber.log.Timber
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,21 +18,29 @@ class RegistrarActivityFragment : Fragment(), QRScannerView.ResultHandler {
     private var mCameraId = -1
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        Timber.d("onCreateView [inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?]")
+
         mScannerView = QRScannerView(this.activity!!)
         return mScannerView as QRScannerView
     }
 
     override fun handleResult(image: ByteArray?, result: Result) {
+        Timber.d("handleResult [image: ByteArray?, result: Result]")
+
         Toast.makeText(this.context!!, result.text, Toast.LENGTH_SHORT).show()
     }
 
     override fun onResume() {
+        Timber.d("onResume")
+
         super.onResume()
         mScannerView?.mResultHandler = this
         mScannerView?.startCamera(mCameraId)
     }
 
     override fun onPause() {
+        Timber.d("onPause")
+
         super.onPause()
         mScannerView?.stopCamera()
     }
