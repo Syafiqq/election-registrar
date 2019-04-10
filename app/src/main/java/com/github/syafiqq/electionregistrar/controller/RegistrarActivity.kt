b861c3081c6a3019f9058f45b848e8252b955a2c
Syafiqq <syafiq.rezpector@gmail.com>
2019-04-10 12:33:13 +0700

    Add Log

--- app/src/main/java/com/github/syafiqq/electionregistrar/controller/RegistrarActivity.kt
+++ app/src/main/java/com/github/syafiqq/electionregistrar/controller/RegistrarActivity.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.github.syafiqq.electionregistrar.R
 import kotlinx.android.synthetic.main.activity_registrar.*
+import timber.log.Timber
 
 class RegistrarActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        Timber.d("onCreate [savedInstanceState: Bundle?]")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registrar)
         setSupportActionBar(toolbar)
--- app/src/main/java/com/github/syafiqq/electionregistrar/controller/RegistrarActivityFragment.kt
+++ app/src/main/java/com/github/syafiqq/electionregistrar/controller/RegistrarActivityFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.github.syafiqq.electionregistrar.R
+import timber.log.Timber
 
 /**
  * A placeholder fragment containing a simple view.
@@ -16,6 +17,7 @@ class RegistrarActivityFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        Timber.d("onCreateView [inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?]")
         return inflater.inflate(R.layout.fragment_registrar, container, false)
     }
 }
