86cda77f5287b2ec83d6cd61550e3c6b65fd7d3b
Syafiqq <syafiq.rezpector@gmail.com>
2019-04-10 13:37:48 +0700

    Fixing onPreviewFrame

--- app/src/main/java/com/github/syafiqq/electionregistrar/model/view/QRScannerView.kt
+++ app/src/main/java/com/github/syafiqq/electionregistrar/model/view/QRScannerView.kt
@@ -2,80 +2,138 @@ package com.github.syafiqq.electionregistrar.model.view
 
 
 import android.content.Context
-import android.graphics.ImageFormat
-import android.graphics.YuvImage
+import android.content.res.Configuration
 import android.hardware.Camera
 import android.os.Handler
 import android.os.Looper
 import android.util.AttributeSet
-import android.util.Log
 import com.github.syafiqq.electionregistrar.model.finder.SquareFinderView
-import com.google.zxing.Result
+import com.google.zxing.*
+import com.google.zxing.common.HybridBinarizer
+import com.google.zxing.qrcode.QRCodeReader
 import me.dm7.barcodescanner.core.BarcodeScannerView
+import me.dm7.barcodescanner.core.DisplayUtils
 import me.dm7.barcodescanner.core.IViewFinder
-import java.io.ByteArrayOutputStream
+import timber.log.Timber
+import java.util.*
 
 
 class QRScannerView : BarcodeScannerView {
-    var mResultHandler: ResultHandler? = null
+    private var mResultHandler: ResultHandler? = null
+    private val hints: EnumMap<DecodeHintType, Any> = EnumMap(DecodeHintType::class.java)
+
+    private val reader: QRCodeReader = QRCodeReader()
 
     constructor(context: Context) : super(context)
 
     constructor(context: Context, attributeSet: AttributeSet) : super(context, attributeSet)
 
-    interface ResultHandler {
-        fun handleResult(image: ByteArray?, result: Result)
+    init {
+        hints[DecodeHintType.TRY_HARDER] = java.lang.Boolean.TRUE
     }
 
     override fun createViewFinderView(context: Context?): IViewFinder {
         return SquareFinderView(context!!)
     }
 
-    internal fun <T> send(obj: T, resultHandler: ResultHandler) {
-        val handler = Handler(Looper.getMainLooper())
-        handler.post {
-            // Stopping the preview can take a little long.
-            // So we want to set result handler to null to discard subsequent calls to
-            // onPreviewFrame.
-            mResultHandler = null
-
-            stopCameraPreview()
-            resultHandler.run {
-                when (this) {
-                    else -> {
-                    }
-                }
-            }
-        }
-    }
-
     @Synchronized
     override fun onPreviewFrame(data: ByteArray, camera: Camera) {
+        var data = data
         if (mResultHandler == null) {
             return
         }
         try {
             val parameters = camera.parameters
-            val format = parameters.previewFormat
-            if (format == ImageFormat.NV21 || format == ImageFormat.YUY2 || format == ImageFormat.NV16) {
-                val w = parameters.previewSize.width
-                val h = parameters.previewSize.height
-                val yuvimage = YuvImage(data, format, w, h, null)
-                val rect = getFramingRectInPreview(w, h)
-                val baos = ByteArrayOutputStream()
-                yuvimage.compressToJpeg(rect, 100, baos)
-                val jdata = baos.toByteArray()
-                if (jdata != null) {
-                    send(jdata, mResultHandler!!)
+            val size = parameters.previewSize
+            var width = size.width
+            var height = size.height
+
+            if (DisplayUtils.getScreenOrientation(context) == Configuration.ORIENTATION_PORTRAIT) {
+                val rotationCount = rotationCount
+                if (rotationCount == 1 || rotationCount == 3) {
+                    val tmp = width
+                    width = height
+                    height = tmp
+                }
+                data = getRotatedData(data, camera)
+            }
+
+            var rawResult: Result? = null
+            val source = buildLuminanceSource(data, width, height)
+
+            if (source != null) {
+                var bitmap = BinaryBitmap(HybridBinarizer(source))
+                try {
+                    rawResult = reader.decode(bitmap, hints)
+                } catch (re: ReaderException) {
+                    // continue
+                } catch (npe: NullPointerException) {
+                    // This is terrible
+                } catch (aoe: ArrayIndexOutOfBoundsException) {
+
+                } finally {
+                    reader.reset()
+                }
+
+                if (rawResult == null) {
+                    val invertedSource = source.invert()
+                    bitmap = BinaryBitmap(HybridBinarizer(invertedSource))
+                    try {
+                        rawResult = reader.decode(bitmap, hints)
+                    } catch (e: NotFoundException) {
+                        // continue
+                    } catch (e: FormatException) {
+                        e.printStackTrace()
+                    } catch (e: ChecksumException) {
+                        e.printStackTrace()
+                    } finally {
+                        reader.reset()
+                    }
                 }
             }
+
+            val finalRawResult = rawResult
+
+            if (finalRawResult != null) {
+                val handler = Handler(Looper.getMainLooper())
+                handler.post {
+                    // Stopping the preview can take a little long.
+                    // So we want to set result handler to null to discard subsequent calls to
+                    // onPreviewFrame.
+                    val tmpResultHandler = mResultHandler
+                    mResultHandler = null
+
+                    stopCameraPreview()
+                    tmpResultHandler?.handleResult(null, finalRawResult)
+                }
+            } else {
+                camera.setOneShotPreviewCallback(this)
+            }
         } catch (e: RuntimeException) {
-            // TODO: Terrible hack. It is possible that this method is invoked after camera is released.
-            Log.e(TAG, e.toString(), e)
+            Timber.e(e)
         }
+    }
+
+    private fun buildLuminanceSource(data: ByteArray, width: Int, height: Int): PlanarYUVLuminanceSource? {
+        var rect = getFramingRectInPreview(width, height) ?: return null
+        rect = getScaledFramingRect(rect)
+        var source: PlanarYUVLuminanceSource? = null
+        try {
+            source = PlanarYUVLuminanceSource(
+                data, width, height, rect.left, rect.top,
+                rect.width(), rect.height(), false
+            )
+        } catch (e: Exception) {
+        }
+
+        return source
     }
 
     companion object {
         private val TAG = "QRScannerView"
+    }
+
+    interface ResultHandler {
+        fun handleResult(image: ByteArray?, result: Result)
     }
 }
